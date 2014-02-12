30a688b9fd04a4bf42f6a0806aa65dd5d955db58
Vaidas Nargėlas <vaidasn@gmail.com>
2014-02-12 22:38:56 +0200

    Reformatted the script

--- git-gvim-config-global.sh
+++ git-gvim-config-global.sh
@@ -2,7 +2,9 @@
 
 git config --global alias.vshow 'show --no-color'
 git config --global alias.vdiff 'diff --no-color'
-git config --global alias.edit '!e() { [[ -n $1 ]] && { exec gvim -n --servername $(git rev-parse --show-toplevel) --remote-tab-silent "$@" & } || exec gvim -n --servername $(git rev-parse --show-toplevel) & }; e'
+git config --global alias.edit '!e() { [[ -n $1 ]] && '`
+                               `'{ exec gvim -n --servername $(git rev-parse --show-toplevel) --remote-tab-silent "$@" & } || '`
+                               `'exec gvim -n --servername $(git rev-parse --show-toplevel) & }; e'
 git config --global pager.vshow 'gvim -R -M -f -'
 git config --global pager.vdiff 'gvim -R -M -f -'
 git config --global core.editor 'gvim -f'
