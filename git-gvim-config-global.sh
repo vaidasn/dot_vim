890acda4967d14473f64204d9e714a2994ddc56f
Vaidas Nargėlas <vaidasn@gmail.com>
2014-02-17 14:03:40 +0200

    Fixed git edit to work not only in git top level directories

--- git-gvim-config-global.sh
+++ git-gvim-config-global.sh
@@ -3,7 +3,8 @@
 git config --global alias.vshow 'show --no-color'
 git config --global alias.vdiff 'diff --no-color'
 git config --global alias.edit '!e() { s="$(git rev-parse --show-toplevel 2>/dev/null)"; [[ -z $s ]] && s=GVIM; '`
-                               `'[[ -n $1 ]] && { exec gvim -n --servername $s --remote-tab-silent "$@" & } || '`
+                               `'[[ -n $1 ]] && '`
+                               `'{ cd "${GIT_PREFIX:-.}"; exec gvim -n --servername $s --remote-tab-silent "$@" & } || '`
                                `'exec gvim -n --servername $s & }; e'
 git config --global pager.vshow 'gvim -R -M -f -'
 git config --global pager.vdiff 'gvim -R -M -f -'
